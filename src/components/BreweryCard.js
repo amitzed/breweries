9a30453888b0abde8895d007f825d8d4d01d3ef3
amitzed <amitzmn@gmail.com>
2020-07-16 21:35:54 -0400

    Added componentDidMount Lifecycle method to BreweryCard so that that right when component is rendered on screen, the Action Creator is called

--- src/components/BreweryCard.js
+++ src/components/BreweryCard.js
@@ -4,6 +4,10 @@ import { connect } from 'react-redux';
 import { fetchBreweries } from '../actions';
 
 class BreweryCard extends React.Component {
+  componentDidMount() {
+    this.props.fetchBreweries()
+  }
+
   render() {
     return (
       <div>BreweryCard</div>
