84aeeaea68eff6c28c1da08d4e3175b2b5916d6d
amitzed <amitzmn@gmail.com>
2020-07-16 21:11:49 -0400

    Created new component BreweryCard to list breweries

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Container, Header, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react';
 
-
+import BreweryCard from './BreweryCard';
 
 const App = () => {
   return (
     <Container>
-      App
+      <BreweryCard />
     </Container>
   );
 }
---
+++ src/components/BreweryCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class BreweryCard extends React.Component {
+  render() {
+    return (
+      <div>BreweryCard</div>
+    )
+  }
+}
+
+export default BreweryCard;
